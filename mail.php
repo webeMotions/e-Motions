a36440b260597e5c67c850df44fd3046c3da13df
webeMotions <88138582+webeMotions@users.noreply.github.com>
2021-08-02 19:24:48 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "webemotions@gmail.com";
+$to = "webemotions21@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@e-motions.online" . "\r\n" .
